ed9a2b776009fbcfe3e8b8ff61fbf78a2f85ee52
Matteo Boni <bonimat@hotmail.com>
2017-06-13 19:52:12 +0200

    Aggiunto il docker-compose

--- site.pp
+++ site.pp
@@ -6,4 +6,12 @@ include ::google_chrome
 include ::jdk_oracle
 include ::netbeans
 include ::pulse
+class { 'python' :
+  version    => 'system',
+  pip        => 'present',
+  dev        => 'absent',
+  virtualenv => 'absent',
+  gunicorn   => 'absent',
+}
 include ::docker
+include ::docker_compose
