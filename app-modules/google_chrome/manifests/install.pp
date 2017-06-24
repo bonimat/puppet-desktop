4fbaaab5edde3357d8c654e0fbbbb68c6c697040
Your Name <you@example.com>
2017-06-24 12:11:24 +0200

    Modificato installazione di chrome

--- app-modules/google_chrome/manifests/install.pp
+++ app-modules/google_chrome/manifests/install.pp
@@ -1,5 +1,5 @@
 class google_chrome::install() inherits google_chrome::params {
-  package { "${google_chrome::params::package_name}-${google_chrome::version}":,
+  package { "google-chrome-stable":,
     ensure => $google_chrome::params::ensure,
   }
 }
